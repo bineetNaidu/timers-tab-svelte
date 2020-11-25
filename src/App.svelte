d45cf72a9e1ac4ba60f78ae3f07bee35be14e493
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-25 16:34:44 +0530

    add base functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+  let time: number = 0;
+
+  $: counterTime = 0;
+
+  let unSub;
+
+  const handleSubmit = () => {
+    counterTime = time;
+    unSub = setInterval(() => {
+      counterTime = counterTime - 1;
+      if (unSub && counterTime === 0) {
+        clearInterval(unSub);
+      }
+    }, 1000);
+  };
+
+  const handleReset = () => {
+    time = 0;
+    counterTime = 0;
+    clearInterval(unSub);
+  };
 </script>
 
 <style>
@@ -12,4 +33,11 @@
 
 <main class="container">
   <h1 class="text-center">Svelte Timers Tab!</h1>
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="number" bind:value={time} step={1} placeholder="*in Secs" />
+    <button type="submit">Start</button>
+  </form>
+
+  <h1>{counterTime}</h1>
+  <button on:click={handleReset}>Reset</button>
 </main>
